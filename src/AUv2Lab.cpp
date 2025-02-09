029d7bf4bedfd61b05bd2959aed8d4d039bd1f95
Izmar Verhage <izmaelverhage@gmail.com>
2025-02-09 20:07:32 +0100

    Set up a mix of mono and stereo buses

--- src/AUv2Lab.cpp
+++ src/AUv2Lab.cpp
@@ -14,7 +14,7 @@ AUSDK_EXPORT extern "C" void* AUv2LabFactory (AudioComponentDescription* inDesc)
 }
 
 AUv2Lab::AUv2Lab(AudioUnit inComponentInstance)
-: ausdk::MusicDeviceBase(inComponentInstance, 1, 5)
+: ausdk::MusicDeviceBase(inComponentInstance, 1, 13)
 {
 #if ENABLE_LOG == 1
     printf("============ AUv2Lab constructor =========\n");
@@ -24,9 +24,8 @@ AUv2Lab::AUv2Lab(AudioUnit inComponentInstance)
 
 AudioStreamBasicDescription AUv2Lab::GetStreamFormat(AudioUnitScope inScope, AudioUnitElement inElement)
 {
-    const auto r = MusicDeviceBase::GetStreamFormat(inScope, inElement);
-
 #if ENABLE_LOG == 1
+    const auto r = MusicDeviceBase::GetStreamFormat(inScope, inElement);
     printf("Desired format -> ID: %u, Flags: 0x%08X, SampleRate: %.2f, Channels: %u, BitsPerChannel: %u, "
            "BytesPerFrame: %u, BytesPerPacket: %u, FramesPerPacket: %u, Reserved: %u\n",
            r.mFormatID, r.mFormatFlags, r.mSampleRate, r.mChannelsPerFrame, r.mBitsPerChannel,
@@ -34,12 +33,27 @@ AudioStreamBasicDescription AUv2Lab::GetStreamFormat(AudioUnitScope inScope, Aud
 #endif
 
     AudioStreamBasicDescription format = {0};
+    
+    if (inScope != kAudioUnitScope_Input && inScope != kAudioUnitScope_Output)
+    {
+        return format;
+    }
+    
+    if (inScope == kAudioUnitScope_Input && inElement >= 1)
+    {
+        return format;
+    }
+    
+    if (inScope == kAudioUnitScope_Output && inElement >= 13)
+    {
+        return format;
+    }
 
     format.mSampleRate = 44100;
     format.mFormatID = kAudioFormatLinearPCM;
     format.mFormatFlags = kAudioFormatFlagIsFloat | kAudioFormatFlagIsPacked | kAudioFormatFlagIsNonInterleaved;
     format.mBitsPerChannel = 32;
-    format.mChannelsPerFrame = inElement == 0 ? 2 : 1;
+    format.mChannelsPerFrame = (inElement >= 0 && inElement <= 4) ? 2 : 1;
 
     // We don't multiply by number of channels, because the stream is non-interleaved.
     // See https://developer.apple.com/documentation/coreaudiotypes/
@@ -56,20 +70,6 @@ AudioStreamBasicDescription AUv2Lab::GetStreamFormat(AudioUnitScope inScope, Aud
            format.mBytesPerFrame, format.mBytesPerPacket, format.mFramesPerPacket, format.mReserved);
 #endif
 
-    if (inScope == kAudioUnitScope_Input && inElement == 0) {
-        format.mChannelsPerFrame = 2;
-    } else if (inScope == kAudioUnitScope_Output) {
-        if (inElement == 0) {
-            format.mChannelsPerFrame = 2;
-        } else if (inElement >= 1 && inElement <= 4) {
-            format.mChannelsPerFrame = 1;
-        } else {
-            return MusicDeviceBase::GetStreamFormat(inScope, inElement);
-        }
-    } else {
-        return MusicDeviceBase::GetStreamFormat(inScope, inElement);
-    }
-
     return format;
 }
 
@@ -83,7 +83,7 @@ bool AUv2Lab::ValidFormat(AudioUnitScope inScope, AudioUnitElement inElement,
 {
     if (inScope != kAudioUnitScope_Output && inScope != kAudioUnitScope_Input)
     {
-        return;
+        return false;
     }
     
     if (inScope == kAudioUnitScope_Input)
@@ -91,8 +91,8 @@ bool AUv2Lab::ValidFormat(AudioUnitScope inScope, AudioUnitElement inElement,
         return inElement == 0 && inNewFormat.mChannelsPerFrame == 2;
     }
     
-    return (inElement == 0 && inNewFormat.mChannelsPerFrame == 2) ||
-    (inElement <= 4 && inNewFormat.mChannelsPerFrame == 1);
+    return (inElement >= 0 && inElement <= 4 && inNewFormat.mChannelsPerFrame == 2) ||
+           (inElement >= 5 && inElement <= 12 && inNewFormat.mChannelsPerFrame == 1);
 }
 
 ComponentResult AUv2Lab::Render (AudioUnitRenderActionFlags& ioActionFlags,
