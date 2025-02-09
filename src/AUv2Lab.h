0ab755cacdcce1f15802439127a126055fe3833e
Izmar Verhage <izmaelverhage@gmail.com>
2025-02-09 21:44:40 +0100

    Demo to show that Logic ignores much of the layout
    Even though AU provides enough API for the host to query what kind
    of layouts are possible, Logic fails to implement such a thing.
    
    In this research project, we try to build an aumu that can be:
    
    1. An instance with 1x stereo in, 1x stereo out.
    2. An instance with 1x stereo in, 5x stereo out.
    3. An instance with 1x stereo in, 1x stereo out, 8x mono out.
    4. An instance with 1x stereo in, 5x stereo out, 8x mono out.
    
    All of the following methods with their mentioned parameters have
    a say in this:
    
    * MusicDeviceBase constructor: UInt32 numInputs, UInt32 numOutputs
    * bool ValidFormat: scope (or in/out), element (or bus)
    * GetStreamFormat: scope (or in/out), element (or bus)
    * StreamFormatWritable: scope (or in/out), element (or bus)
    * GetChannelLayoutTags:  scope (or in/out), element (or bus)
    * SupportedNumChannels: This is an odd one. My current take is to
      return an array with all possible global i/o channel count pairs
      that your component supports. For the above, the following would
      be returned: {{2,2},{2,10},{2,18}}. That supports all mentioned
      buses configurations. There are some tricks with negative values
      that you can do here, see Apple docs. But for enforcing a strict
      set of layouts, my current understanding is that the AU SDK
      wants us to return the value I just mentioned.
    * SetBusCount: scope (or in/out), count (number of buses)
    * SetAudioChannelLayout: scope, element, layout
    * GetAudioChannelLayout: scope, element
    
    These API methods and their inputs and outputs together give the
    host enough resources to figure out the 4 instance configurations.
    
    Interestingly, Logic figures out a relatively sane set of instance
    configurations if we only provide what auval refers to as a
    "default layout". This is done by not explicitly providing any
    layouts, which in turn is done by removing GetChannelLayoutTags
    and SupportedNumChannels to rely on the super class.
    
    reuk has already analysed this before, and described in this
    comment:
    https://github.com/juce-framework/JUCE/issues/1508#issuecomment-2645937780
    
    By relying on the default layout, we can at least get Logic to
    offer these instances:
    
    1. An instance with 1x stereo in, 1x stereo out.
    2. An instance with 1x stereo in, 5x stereo out, 8 mono out.

--- src/AUv2Lab.h
+++ src/AUv2Lab.h
@@ -1,6 +1,8 @@
 #include <AudioUnitSDK/MusicDeviceBase.h>
 #include <Carbon/Carbon.h>
 
+#include <iostream>
+
 class AUv2Lab : public ausdk::MusicDeviceBase
 {
 public:
@@ -25,13 +27,136 @@ public:
     
     std::vector<AudioChannelLayoutTag> GetChannelLayoutTags (AudioUnitScope inScope, AudioUnitElement inElement) override
     {
-        return {};
+        if (inScope == kAudioUnitScope_Input)
+        {
+            if (inElement == 0)
+            {
+                return {kAudioChannelLayoutTag_Stereo};
+            }
+        }
+        if (inScope == kAudioUnitScope_Output)
+        {
+            if (inElement == 0)
+            {
+                return {kAudioChannelLayoutTag_Stereo};
+            }
+            if (inElement >= 1 && inElement <= 4)
+            {
+                return {kAudioChannelLayoutTag_Stereo};
+            }
+            if (inElement >= 5 && inElement <= 12)
+            {
+                return {kAudioChannelLayoutTag_Mono};
+            }
+        }
+                
+        return {kAudioChannelLayoutTag_Unknown | 0};
+    }
+    
+    UInt32 SupportedNumChannels(const AUChannelInfo** outInfo) override {
+        static const AUChannelInfo channelInfos[] = {
+            {2, 2},   // Stereo in → Stereo out
+              {2, 10},  // Stereo in → 5x Stereo out, as well as
+                        // Stereo in → 1x Stereo out, 8 Mono out
+              {2, 18}   // Stereo in → 5x Stereo out + 8 Mono out
+        };
+
+        if (outInfo) *outInfo = channelInfos;
+        return 3;
     }
 
-    OSStatus SetAudioChannelLayout (AudioUnitScope scope, AudioUnitElement element, const AudioChannelLayout* inLayout) override
+    
+    OSStatus SetBusCount(AudioUnitScope inScope, UInt32 inCount) override
     {
+        if (IsInitialized()) {
+            return kAudioUnitErr_Initialized;
+        }
+        
+        if ((inScope == kAudioUnitScope_Input && inCount != 1) ||
+            (inScope == kAudioUnitScope_Output && inCount != 1 && inCount != 5 && inCount != 9))
+        {
+            return kAudioUnitErr_InvalidParameterValue;
+        }
+
+        GetScope(inScope).SetNumberOfElements(inCount);
         return noErr;
     }
+    
+    OSStatus SetAudioChannelLayout(AudioUnitScope scope, AudioUnitElement element, const AudioChannelLayout* inLayout) override
+    {
+        std::cout << "SetAudioChannelLayout called - Scope: " << scope
+                  << ", Element: " << element
+                  << ", LayoutTag: " << (inLayout ? inLayout->mChannelLayoutTag : -1) << std::endl;
+
+        if (scope == kAudioUnitScope_Input && element >= 1)
+        {
+            return kAudioUnitErr_InvalidElement;
+        }
+        if (scope == kAudioUnitScope_Output && element >= 13)
+        {
+            return kAudioUnitErr_InvalidElement;
+        }
+        
+        const auto tag = inLayout->mChannelLayoutTag;
+        
+        if (scope == kAudioUnitScope_Input && tag != kAudioChannelLayoutTag_Stereo)
+        {
+            return kAudioUnitErr_InvalidParameterValue;
+        }
+        
+        if (scope == kAudioUnitScope_Output && element <= 4 && tag != kAudioChannelLayoutTag_Stereo)
+        {
+            return kAudioUnitErr_InvalidParameterValue;
+        }
+        
+        if (scope == kAudioUnitScope_Output && element >= 5 && element <= 12 && tag != kAudioChannelLayoutTag_Mono)
+        {
+            return kAudioUnitErr_InvalidParameterValue;
+        }
+        
+        return noErr;
+    }
+
+    UInt32 GetAudioChannelLayout(AudioUnitScope scope, AudioUnitElement element,
+        AudioChannelLayout* outLayoutPtr, bool& outWritable) override
+    {
+        outWritable = false; // Fixed layouts, host cannot modify
+
+        if (scope == kAudioUnitScope_Input)
+        {
+            if (element == 0) // Single input bus (Stereo)
+            {
+                if (!outLayoutPtr) return sizeof(AudioChannelLayout);
+
+                outLayoutPtr->mChannelLayoutTag = kAudioChannelLayoutTag_Stereo;
+                outLayoutPtr->mNumberChannelDescriptions = 0;
+                return sizeof(AudioChannelLayout);
+            }
+            return 0; // No other input buses
+        }
+
+        if (scope == kAudioUnitScope_Output)
+        {
+            UInt32 layoutSize = sizeof(AudioChannelLayout);
+            AudioChannelLayoutTag layoutTag;
+
+            if (element >= 0 && element <= 4)
+                layoutTag = kAudioChannelLayoutTag_Stereo;
+            else if (element >= 5 && element <= 12)
+                layoutTag = kAudioChannelLayoutTag_Mono;
+            else
+                return 0; // No valid layout
+
+            if (!outLayoutPtr) return layoutSize;
+
+            outLayoutPtr->mChannelLayoutTag = layoutTag;
+            outLayoutPtr->mNumberChannelDescriptions = 0;
+            return layoutSize;
+        }
+
+        return 0; // Invalid scope
+    }
+
         
     ComponentResult GetPropertyInfo(AudioUnitPropertyID inID,
                                     AudioUnitScope inScope,
