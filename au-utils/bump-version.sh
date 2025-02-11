314f10fc4989d46a3bf24259f440e8f334bc9ba3
Izmar Verhage <izmaelverhage@gmail.com>
2025-02-11 15:40:34 +0100

    Fix version bump script

--- au-utils/bump-version.sh
+++ au-utils/bump-version.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-PLIST_FILE="./build-resources/AUv3/Info.plist"
+PLIST_FILE="../build-resources/AUv3/Info.plist"
 
-CURRENT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :AudioComponents:0:version" "$PLIST_FILE")
-NEW_VERSION=$((CURRENT_VERSION + 1))
-
-/usr/libexec/PlistBuddy -c "Set :AudioComponents:0:version $NEW_VERSION" "$PLIST_FILE"
-
-echo "Updated version component 0: $NEW_VERSION"
-
-CURRENT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :AudioComponents:1:version" "$PLIST_FILE")
-NEW_VERSION=$((CURRENT_VERSION + 1))
-
-/usr/libexec/PlistBuddy -c "Set :AudioComponents:1:version $NEW_VERSION" "$PLIST_FILE"
-
-echo "Updated version component 1: $NEW_VERSION"
-
+for i in 0 1; do
+    CURRENT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :NSExtension:NSExtensionAttributes:AudioComponents:${i}:version" "$PLIST_FILE")
+    NEW_VERSION=$((CURRENT_VERSION + 1))
+    /usr/libexec/PlistBuddy -c "Set :NSExtension:NSExtensionAttributes:AudioComponents:${i}:version $NEW_VERSION" "$PLIST_FILE"
+    echo "Updated version component ${i}: $NEW_VERSION"
+done
